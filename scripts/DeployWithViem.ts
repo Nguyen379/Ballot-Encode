a5c36d8a33fa77f22506e7b27bc332dded077424
Nguyen379 <anhnguyenle379@gmail.com>
2024-08-22 23:08:17 -0500

    Deploy script

--- scripts/DeployWithViem.ts
+++ scripts/DeployWithViem.ts
@@ -3,7 +3,7 @@ import { sepolia } from "viem/chains";
 import * as dotenv from "dotenv";
 import { privateKeyToAccount } from "viem/accounts";
 import { abi, bytecode } from "../artifacts/contracts/Ballot.sol/Ballot.json";
-import { toHex, hexToString } from "viem";
+import { toHex, hexToString, parseEther } from "viem";
 
 
 dotenv.config();
@@ -12,15 +12,15 @@ const providerApiKey = process.env.ALCHEMY_API_KEY || "";
 const deployerPrivateKey = process.env.PRIVATE_KEY || "";
 
 async function main() {
-  const proposals = process.argv.slice(2);
-  if (!proposals || proposals.length < 1)
-    throw new Error("Proposals not provided");
-  const publicClient = createPublicClient({
-    chain: sepolia,
-    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
-  });
-  const blockNumber = await publicClient.getBlockNumber();
-  console.log("Last block number:", blockNumber);
+	const proposals = process.argv.slice(2);
+	if (!proposals || proposals.length < 1)
+		throw new Error("Proposals not provided");
+	const publicClient = createPublicClient({
+		chain: sepolia,
+		transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+	});
+	const blockNumber = await publicClient.getBlockNumber();
+	console.log("Last block number:", blockNumber);
 
 	const account = privateKeyToAccount(`0x${deployerPrivateKey}`);
 	const deployer = createWalletClient({
@@ -38,9 +38,13 @@ async function main() {
     "Deployer balance:",
     formatEther(balance),
     deployer.chain.nativeCurrency.symbol
-  );
+	);
 
-  console.log("\nDeploying Ballot contract");
+	if (balance < parseEther("0.001")) {
+		throw new Error("Insufficient balance to deploy contract");
+	}
+
+	console.log("\nDeploying Ballot contract");
 	const hash = await deployer.deployContract({
 		abi,
 		bytecode: bytecode as `0x${string}`, 
@@ -54,20 +58,20 @@ async function main() {
 
 	if (!receipt.contractAddress) throw new Error("Contract address not found");
 	console.log("Proposals: ");
-  for (let index = 0; index < proposals.length; index++) {
-    const proposal = (await publicClient.readContract({
-      address: receipt.contractAddress,
-      abi,
-      functionName: "proposals",
-      args: [BigInt(index)],
-    })) as any[];
-    const name = hexToString(proposal[0], { size: 32 });
-    console.log({ index, name, proposal });
-  }
+	for (let index = 0; index < proposals.length; index++) {
+		const proposal = (await publicClient.readContract({
+			address: receipt.contractAddress,
+			abi,
+			functionName: "proposals",
+			args: [BigInt(index)],
+		})) as any[];
+		const name = hexToString(proposal[0], { size: 32 });
+		console.log({ index, name, proposal });
+	}
 }
 
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
--- test/Ballot.test.ts
+++ test/Ballot.test.ts
@@ -21,117 +21,117 @@ async function deployContractFixture() {
 }
 
 describe("Ballot", async () => {
-  describe("when the contract is deployed", async () => {
-    it("has the provided proposals", async () => {
-			const {ballotContract} = await loadFixture(deployContractFixture);
-			for (let i = 0; i < PROPOSALS.length; i++) {
-				const proposal = await ballotContract.read.proposals([BigInt(i)]);
-				expect(hexToString(proposal[0], {size: 32})).to.equal(PROPOSALS[i]);
-			}
+    describe("when the contract is deployed", async () => {
+        it("has the provided proposals", async () => {
+            const {ballotContract} = await loadFixture(deployContractFixture);
+            for (let i = 0; i < PROPOSALS.length; i++) {
+                const proposal = await ballotContract.read.proposals([BigInt(i)]);
+            expect(hexToString(proposal[0], {size: 32})).to.equal(PROPOSALS[i]);
+        }
+        });
+
+        it("has zero votes for all proposals", async () => {
+                const {ballotContract} = await loadFixture(deployContractFixture);
+        for (let index = 0; index < PROPOSALS.length; index++) {
+            const proposal = await ballotContract.read.proposals([BigInt(index)]);
+            expect(proposal[1]).to.eq(0n);
+        }
+        });
+        it("sets the deployer address as chairperson", async () => {
+            const {ballotContract, owner} = await loadFixture(deployContractFixture);
+            const chairperson = await ballotContract.read.chairperson();
+            expect(chairperson.toLowerCase()).to.equal(owner.account.address);
+        });
+        it("sets the voting weight for the chairperson as 1", async () => {
+            const {ballotContract, owner} = await loadFixture(deployContractFixture);
+            const chairperson = await ballotContract.read.chairperson();
+            const chairpersonVoter = await ballotContract.read.voters([chairperson]);
+            expect(chairpersonVoter[0]).to.eq(1n);
+        });
     });
 
-    it("has zero votes for all proposals", async () => {
-			const {ballotContract} = await loadFixture(deployContractFixture);
-      for (let index = 0; index < PROPOSALS.length; index++) {
-        const proposal = await ballotContract.read.proposals([BigInt(index)]);
-        expect(proposal[1]).to.eq(0n);
-      }
+    describe("when the chairperson interacts with the giveRightToVote function in the contract", async () => {
+        it("gives right to vote for another address", async () => {
+        // TODO
+        throw Error("Not implemented");
+        });
+        it("can not give right to vote for someone that has voted", async () => {
+        // TODO
+        throw Error("Not implemented");
+        });
+        it("can not give right to vote for someone that has already voting rights", async () => {
+        // TODO
+        throw Error("Not implemented");
+        });
     });
-    it("sets the deployer address as chairperson", async () => {
-      const {ballotContract, owner} = await loadFixture(deployContractFixture);
-      const chairperson = await ballotContract.read.chairperson();
-      expect(chairperson.toLowerCase()).to.equal(owner.account.address);
-    });
-    it("sets the voting weight for the chairperson as 1", async () => {
-      const {ballotContract, owner} = await loadFixture(deployContractFixture);
-      const chairperson = await ballotContract.read.chairperson();
-      const chairpersonVoter = await ballotContract.read.voters([chairperson]);
-      expect(chairpersonVoter[0]).to.eq(1n);
-    });
-  });
 
-  describe("when the chairperson interacts with the giveRightToVote function in the contract", async () => {
-    it("gives right to vote for another address", async () => {
-      // TODO
-      throw Error("Not implemented");
+    describe("when the voter interacts with the vote function in the contract", async () => {
+        // TODO
+        it("should register the vote", async () => {
+        throw Error("Not implemented");
+        });
     });
-    it("can not give right to vote for someone that has voted", async () => {
-      // TODO
-      throw Error("Not implemented");
-    });
-    it("can not give right to vote for someone that has already voting rights", async () => {
-      // TODO
-      throw Error("Not implemented");
-    });
-  });
 
-  describe("when the voter interacts with the vote function in the contract", async () => {
-    // TODO
-    it("should register the vote", async () => {
-      throw Error("Not implemented");
+    describe("when the voter interacts with the delegate function in the contract", async () => {
+        // TODO
+        it("should transfer voting power", async () => {
+        throw Error("Not implemented");
+        });
     });
-  });
 
-  describe("when the voter interacts with the delegate function in the contract", async () => {
-    // TODO
-    it("should transfer voting power", async () => {
-      throw Error("Not implemented");
+    describe("when an account other than the chairperson interacts with the giveRightToVote function in the contract", async () => {
+        // TODO
+        it("should revert", async () => {
+        throw Error("Not implemented");
+        });
     });
-  });
 
-  describe("when an account other than the chairperson interacts with the giveRightToVote function in the contract", async () => {
-    // TODO
-    it("should revert", async () => {
-      throw Error("Not implemented");
+    describe("when an account without right to vote interacts with the vote function in the contract", async () => {
+        // TODO
+        it("should revert", async () => {
+        throw Error("Not implemented");
+        });
     });
-  });
 
-  describe("when an account without right to vote interacts with the vote function in the contract", async () => {
-    // TODO
-    it("should revert", async () => {
-      throw Error("Not implemented");
+    describe("when an account without right to vote interacts with the delegate function in the contract", async () => {
+        // TODO
+        it("should revert", async () => {
+        throw Error("Not implemented");
+        });
     });
-  });
 
-  describe("when an account without right to vote interacts with the delegate function in the contract", async () => {
-    // TODO
-    it("should revert", async () => {
-      throw Error("Not implemented");
+    describe("when someone interacts with the winningProposal function before any votes are cast", async () => {
+        // TODO
+        it("should return 0", async () => {
+        throw Error("Not implemented");
+        });
     });
-  });
 
-  describe("when someone interacts with the winningProposal function before any votes are cast", async () => {
-    // TODO
-    it("should return 0", async () => {
-      throw Error("Not implemented");
+    describe("when someone interacts with the winningProposal function after one vote is cast for the first proposal", async () => {
+        // TODO
+        it("should return 0", async () => {
+        throw Error("Not implemented");
+        });
     });
-  });
 
-  describe("when someone interacts with the winningProposal function after one vote is cast for the first proposal", async () => {
-    // TODO
-    it("should return 0", async () => {
-      throw Error("Not implemented");
+    describe("when someone interacts with the winnerName function before any votes are cast", async () => {
+        // TODO
+        it("should return name of proposal 0", async () => {
+        throw Error("Not implemented");
+        });
     });
-  });
 
-  describe("when someone interacts with the winnerName function before any votes are cast", async () => {
-    // TODO
-    it("should return name of proposal 0", async () => {
-      throw Error("Not implemented");
+    describe("when someone interacts with the winnerName function after one vote is cast for the first proposal", async () => {
+        // TODO
+        it("should return name of proposal 0", async () => {
+        throw Error("Not implemented");
+        });
     });
-  });
 
-  describe("when someone interacts with the winnerName function after one vote is cast for the first proposal", async () => {
-    // TODO
-    it("should return name of proposal 0", async () => {
-      throw Error("Not implemented");
+    describe("when someone interacts with the winningProposal function and winnerName after 5 random votes are cast for the proposals", async () => {
+        // TODO
+        it("should return the name of the winner proposal", async () => {
+        throw Error("Not implemented");
+        });
     });
-  });
-
-  describe("when someone interacts with the winningProposal function and winnerName after 5 random votes are cast for the proposals", async () => {
-    // TODO
-    it("should return the name of the winner proposal", async () => {
-      throw Error("Not implemented");
-    });
-  });
 });
